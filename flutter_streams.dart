97783c729812977e465352f9df371b41715d317b
Nia Cubilla <60896546+niacubilla@users.noreply.github.com>
2021-01-04 17:37:18 -0500

    Corrección de Adolfo Cueto

--- flutter_streams.dart
+++ flutter_streams.dart
@@ -4,78 +4,85 @@ import 'package:flutter/material.dart';
 
 class ItemModel {
 	final StreamController<int> _itemsController = StreamController<int>.broadcast();
+	final StreamController<int> _receiverController = StreamController<int>.broadcast();
 	StreamSubscription<int> _itemsSubscription;
 	Stream<int> get outItems => _itemsController.stream;
 	Sink<int> get inItems => _itemsController.sink;
 
 	void dispose() {
 		_itemsController.close();
+		_receiverController.close();
 	}
 
-	stream() async {
-		while (true) {
+  	stream() async {
+    	_itemsSubscription = _receiverController.stream.listen((value) {
+			inItems.add(value);
+			print("_itemsSubscription:" + value.toString());
+		});
+
+		while ( true ) {
 			await Future.delayed(Duration(seconds: 1));
 			var value = Random().nextInt(20000);
-			inItems.add(value);
-
-			_itemsSubscription = outItems.listen((value) {
-				print(value);
-			});
+			_receiverController.sink.add(value);
+			print("_receiverController:" + value.toString());
 		}
 	}
 
 	pause() {
-		//_itemsSubscription = outItems.listen((value) => print(value));
 		_itemsSubscription.pause();
 	}
+
 	resume() {
-		//_itemsSubscription = outItems.listen((value) => print(value));
 		_itemsSubscription.resume();
 	}
+
 	cancel() {
-		//_itemsSubscription = outItems.listen((value) => print(value));
 		_itemsSubscription.cancel();
+		_itemsController.close();
 	}
 }
 
-class ShopScreen extends StatelessWidget {
+class StreamScreen extends StatelessWidget {
 	final _itemModel = ItemModel();
-	
+
 	@override
 	Widget build(BuildContext context) {
-		_itemModel.stream();
 		return Scaffold(
 			body: Center(
 				child: Column(
-				  	children: [
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: [
 						StreamBuilder(
 							stream: _itemModel.outItems,
-							builder: (context, snapshot) {  
-								if (!snapshot.hasData) {
-									return LoadingWidget();
-								}
-								if (snapshot.connectionState == ConnectionState.done) {
-									return Text("Fin del Stream :(");
-								}
-								return Text(
-									snapshot.hasData ? snapshot.data.toString()
-									: 'No data here',
-									style: TextStyle(fontSize:50),
-								);
+							builder: (context, snapshot) {
+							if (!snapshot.hasData) {
+								return LoadingWidget();
+							}
+							if (snapshot.connectionState == ConnectionState.done) {
+								return Text("Fin del Stream :(");
+							}
+							return Text(
+								snapshot.data.toString(),
+								style: TextStyle(fontSize: 50),
+							);
 							},
 						),
-
-						SizedBox(height:20),
-						PauseButton(),
-
-						SizedBox(height:20),
-						ResumeButton(),
-
-						SizedBox(height:20),
-						CancelButton(),
-
-						SizedBox(height:20),
-						StartButton(),
+						SizedBox(height: 20),
+						PauseButton(
+							itemModel: _itemModel,
+						),
+						SizedBox(height: 20),
+						ResumeButton(
+							itemModel: _itemModel,
+						),
+						SizedBox(height: 20),
+						CancelButton(
+							itemModel: _itemModel,
+						),
+						SizedBox(height: 20),
+						StartButton(
+							itemModel: _itemModel,
+						),
 					],
 				)
 			),
@@ -88,7 +95,7 @@ class LoadingWidget extends StatelessWidget {
 	Widget build(BuildContext context) {
 		return Column(
 			mainAxisAlignment: MainAxisAlignment.center,
-			children: <Widget> [
+			children: <Widget>[
 				Text("Esperando stream"),
 				SizedBox(height: 20),
 				CircularProgressIndicator()
@@ -97,49 +104,61 @@ class LoadingWidget extends StatelessWidget {
 	}
 }
 
-class PauseButton extends StatelessWidget{
-	final _itemModel = ItemModel();
+class PauseButton extends StatelessWidget {
+	final ItemModel itemModel;
+
+	PauseButton({this.itemModel});
+
 	@override
 	Widget build(BuildContext context) {
 		return RaisedButton(
 			color: Colors.orange[300],
-			onPressed: () => _itemModel.pause(),
+			onPressed: () => itemModel.pause(),
 			child: Text("Pause stream"),
 		);
 	}
 }
 
-class ResumeButton extends StatelessWidget{
-	final _itemModel = ItemModel();
+class ResumeButton extends StatelessWidget {
+	final ItemModel itemModel;
+
+	ResumeButton({this.itemModel});
+
 	@override
 	Widget build(BuildContext context) {
 		return RaisedButton(
 			color: Colors.blue[300],
-			onPressed: () => _itemModel.resume(),
+			onPressed: () => itemModel.resume(),
 			child: Text("Resume stream"),
 		);
 	}
 }
 
-class CancelButton extends StatelessWidget{
-	final _itemModel = ItemModel();
+class CancelButton extends StatelessWidget {
+	final ItemModel itemModel;
+
+	CancelButton({this.itemModel});
+
 	@override
 	Widget build(BuildContext context) {
 		return RaisedButton(
 			color: Colors.red[300],
-			onPressed: () => _itemModel.cancel(),
+			onPressed: () => itemModel.cancel(),
 			child: Text("Cancel stream"),
 		);
 	}
 }
 
-class StartButton extends StatelessWidget{
-	final _itemModel = ItemModel();
+class StartButton extends StatelessWidget {
+	final ItemModel itemModel;
+
+	StartButton({this.itemModel});
+
 	@override
 	Widget build(BuildContext context) {
 		return RaisedButton(
 			color: Colors.green[300],
-			onPressed: () => _itemModel.stream(),
+			onPressed: () => itemModel.stream(),
 			child: Text("Start stream"),
 		);
 	}
